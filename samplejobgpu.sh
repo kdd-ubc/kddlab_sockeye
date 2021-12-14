7b6fe144aa8c1923d6e5fffe29a6706a106a1342
Khanh Dao Duc <54422868+kdd-ubc@users.noreply.github.com>
2021-12-13 16:02:05 -0800

    Update samplejobgpu.sh

--- samplejobgpu.sh
+++ samplejobgpu.sh
@@ -2,11 +2,11 @@
  
 #PBS -l walltime=72:00:00,select=1:ncpus=12:ompthreads=12:ngpus=2:mem=186gb
 #PBS -N kdd_sockeye_demo                           
-#PBS -A ex-kdd-1-gpu                              
+#PBS -A pr-kdd-1-gpu                              
 #PBS -m abe                                   
 #PBS -M rtkushner@alumni.ubc.ca              
-#PBS -o /scratch/ex-kdd-1/rt/kddlab_sockeye/output.txt
-#PBS -e /scratch/ex-kdd-1/rt/kddlab_sockeye/error.txt
+#PBS -o /scratch/pr-kdd-1/rt/kddlab_sockeye/output.txt
+#PBS -e /scratch/pr-kdd-1/rt/kddlab_sockeye/error.txt
  
 ################################################################################
  
@@ -15,10 +15,10 @@ module load gcc
 module load cuda
 module load python3
 
-cd /arc/project/ex-kdd-1/rt/kddlab_sockeye
+cd /arc/project/pr-kdd-1/rt/kddlab_sockeye
 
 #consume your envs, activate environments
 source testvenv/bin/activate
 
 #run your code here
-python3 experiment_parser.py --outdir /scratch/ex-kdd-1/rt/kddlab_sockeye/ -it 1000000
+python3 experiment_parser.py --outdir /scratch/pr-kdd-1/rt/kddlab_sockeye/ -it 1000000
