65e0bf2d5cd9db6f26c44b1bd495eb65320372a5
Khanh Dao Duc <54422868+kdd-ubc@users.noreply.github.com>
2021-12-13 16:01:28 -0800

    Update samplejob.sh

--- samplejob.sh
+++ samplejob.sh
@@ -2,22 +2,22 @@
  
 #PBS -l walltime=0:20:00,select=1:ncpus=1:mem=2gb
 #PBS -N kdd_sockeye_demo                           
-#PBS -A ex-kdd-1                               
+#PBS -A pr-kdd-1                               
 #PBS -m abe                                   
 #PBS -M rtkushner@alumni.ubc.ca              
-#PBS -o /scratch/ex-kdd-1/rt/kddlab_sockeye/output.txt
-#PBS -e /scratch/ex-kdd-1/rt/kddlab_sockeye/error.txt
+#PBS -o /scratch/pr-kdd-1/rt/kddlab_sockeye/output.txt
+#PBS -e /scratch/pr-kdd-1/rt/kddlab_sockeye/error.txt
  
 ################################################################################
  
 # load your modules
 module load python3
 
-cd /arc/project/ex-kdd-1/rt/kddlab_sockeye
+cd /arc/project/pr-kdd-1/rt/kddlab_sockeye
 
 #consume your envs, activate environments
 source testvenv/bin/activate
 
 
 #run your code here
-python3 experiment_parser.py --outdir /scratch/ex-kdd-1/rt/kddlab_sockeye/ -it 100
+python3 experiment_parser.py --outdir /scratch/pr-kdd-1/rt/kddlab_sockeye/ -it 100
